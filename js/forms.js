bd331a017122fa8f16b4d0cfd83928f8b0893302
vguelere <v.guelere@gmail.com>
2014-05-06 17:16:46 -0300

    Alteração de cores

--- js/forms.js
+++ js/forms.js
@@ -11,7 +11,7 @@
 					successCl:'success',
 					successShow:'4000',
 					mailHandlerURL:'bat/MailHandler.php',
-					ownerEmail:'support@template-help.com',
+					ownerEmail:'v.guelere@gmail.com',
 					stripHTML:true,
 					smtpMailServer:'localhost',
 					targets:'input,textarea',
